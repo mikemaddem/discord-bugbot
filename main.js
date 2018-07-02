8bc145fc1391b4c7ee3ae4defe98f7ea995839e5
Mike M <mikemaddem@gmail.com>
2018-07-01 22:07:20 -0400

    remove hard coded db.run and update db structure

--- main.js
+++ main.js
@@ -2,60 +2,63 @@
  * Send me all of  the shitty bugs
  */
 
+// Import the discord.js module
 const Discord = require('discord.js');
 
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
 // import config
 const config = require("./config.json");
 
+startupsql = `CREATE TABLE IF NOT EXISTS bug_reports (
+    bug_id integer PRIMARY KEY AUTOINCREMENT,
+    reporter text NOT NULL,
+    description text NOT NULL,
+    steps text NOT NULL,
+    client_info text NOT NULL,
+    user_system text NOT NULL,
+    approved integer DEFAULT 0,
+    votes integer DEFAULT 0);`;
+
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./db/main.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
-      console.error(err.message);
+        return console.error(err.message);
     }
-    console.log('Connected to main database.');
-  });
-
-client.on('ready', () => {
-    
-    db.run("CREATE TABLE IF NOT EXISTS `bug_reports` (`id`	INTEGER PRIMARY KEY AUTOINCREMENT, `reported`	text, `description`	BLOB, `approved`	INTEGER DEFAULT 0, `denied`	INTEGER DEFAULT 0, `reporter`	text DEFAULT 'Unknown', `open`	text DEFAULT 'Unknown')");
-    console.log("DB Setup properly");
-    console.log("--------------------------");
-    console.log('Hit me with your worst bugs!');
-    console.log('No. Actually, dont hit me with bugs, thats gross');
-    console.log("-------------------------------------");
+    else{
+        db.run(startupsql);
+    }
+    console.log('Connected to the bugs SQlite database.');
 });
 
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+    console.log('Hit me with your worst bugs!');
+    console.log('No. Actually, dont hit me with bugs, thats gross');
+});
+
+// Create an event listener for messages
 client.on('message', message => {
 
+    
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     // check to see if the message is actually made for the bot
 
     if (command == 'ping') {
-        var number = parseInt(args);
 
-        for(var i=0; i<number; i++){
-            message.channel.send('pong');
-        }
-        
+        message.channel.send('pong');
     }
 
     if (command == 'src' || command == 'source') {
         // Send "pong" to the same channel
-        message.channel.send('https://github.com/mikemaddem/discord-bugjs'+" "+ args);
-    }
-    if(command == 'createissue'){
-        var description = args
-        const datetime = new Date();
-        var reporter = message.author
-        db.run(`INSERT INTO bug_reports (description, reporter) VALUES ("${description}", "${reporter}")`);
-    }
-    if(command == 'commands'){
-        message.channel.send('All of my commands are...');
-        message.channel.send('about, src, meme,');
+        message.channel.send('https://github.com/mikemaddem/discord-bugjs');
     }
     if (command == 'about'){
         message.channel.send('A wise handsome young man by the name of Michael Madden is my creator, he created me' +
@@ -63,11 +66,15 @@ client.on('message', message => {
         message.channel.send('Check him out @ mikemadden.me')
     }
     if (command == 'meme' || command == 'memes'){
-        message.channel.send('no');
-        message.channel.send('memes');
-        message.channel.send('for');
-        message.channel.send('you');
-        message.channel.send(':rofl:')
+        message.channel.send(':flag_ru: I have strict orders to stop memes :flag_ru: ');
+    }
+    if (command == 'report'){
+        // ok shit we have to do some for real shit.
+        
+
+    }
+    else{
+        message.channel.send('Shit Mike didnt teach me this yet. Blame him not me')
     }
 
 });
